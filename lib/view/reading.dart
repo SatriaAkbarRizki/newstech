6fad324cff94d8d4e0a2988085ff83802d8e6524
SatriaAkbarRizki <sartiapl@gmail.com>
2023-10-01 12:13:49 +0700

    add animation in page reading

--- lib/view/reading.dart
+++ lib/view/reading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:newstech/model/news.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -7,10 +8,6 @@ class ReadingNews extends StatelessWidget {
   static const readingRoute = '/ReadingRoute';
   final List<NewsModel> listNews;
   const ReadingNews({required this.listNews, super.key});
-  final title =
-      "Spotify Luncurkan Fitur Terjemahan Bertenaga AI, Gunakan Suara Podcaster Top";
-  final description =
-      "Spotify telah meluncurkan sistem AI yang menerjemahkan podcast populer ke berbagai bahasa dengan tetap mempertahankan suara pembawa acara atau podcaster top. Fitur terjemahan dapat mentranskripsikan bahasa....";
   @override
   Widget build(BuildContext context) {
     final args = ModalRoute.of(context)!.settings.arguments as NewsModel;
@@ -23,12 +20,16 @@ class ReadingNews extends StatelessWidget {
             children: <Widget>[
               Stack(
                 children: [
-                  Container(
-                    width: sizeDevice.width,
-                    height: 350,
-                    child: Image.network(
-                      '${args.image_url}',
-                      fit: BoxFit.cover,
+                  Animate(
+                    effects: [FadeEffect(duration: 800.ms, delay: 500.ms)],
+                    child: Container(
+                      width: sizeDevice.width,
+                      height: 350,
+                      decoration: BoxDecoration(color: Colors.black),
+                      child: Image.network(
+                        '${args.image_url}',
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
                   Container(
@@ -47,22 +48,25 @@ class ReadingNews extends StatelessWidget {
                       ),
                     ),
                   ),
-                  Positioned(
-                      left: 10,
-                      top: 245,
-                      child: SizedBox(
-                        width: sizeDevice.width,
-                        child: Text(
-                          '${args.title}',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontFamily: 'TitleNewsRoslab',
-                            fontWeight: FontWeight.w400,
-                            fontSize: 18,
+                  Animate(
+                    effects: [FadeEffect(duration: 500.ms, delay: 200.ms)],
+                    child: Positioned(
+                        left: 10,
+                        top: 245,
+                        child: SizedBox(
+                          width: sizeDevice.width,
+                          child: Text(
+                            '${args.title}',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'TitleNewsRoslab',
+                              fontWeight: FontWeight.w400,
+                              fontSize: 18,
+                            ),
                           ),
-                        ),
-                      ))
+                        )),
+                  )
                 ],
               )
             ],
@@ -80,13 +84,18 @@ class ReadingNews extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                      padding: EdgeInsets.all(20),
-                      child: Text(
-                        '${args.description}',
-                        style: TextStyle(
-                            fontFamily: 'SourceSans', fontSize: 19, height: 2),
-                      ))
+                  Animate(
+                    effects: [FadeEffect(duration: 800.ms, delay: 350.ms)],
+                    child: Container(
+                        padding: EdgeInsets.all(20),
+                        child: Text(
+                          '${args.description}',
+                          style: TextStyle(
+                              fontFamily: 'SourceSans',
+                              fontSize: 19,
+                              height: 2),
+                        )),
+                  )
                 ],
               ),
             ),
